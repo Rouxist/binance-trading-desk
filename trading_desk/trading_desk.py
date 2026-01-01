22b921ad9fe3fe5bd99ee3bbfeccc1152d0c6e56
Rouxist <name.of.squaner@gmail.com>
2026-01-01 13:13:01 +0000

    feat: added is_mock option

--- trading_desk/trading_desk.py
+++ trading_desk/trading_desk.py
@@ -8,9 +8,12 @@ __all__ = ["TradingDesk"]
 
 
 class TradingDesk:
-    def __init__(self, is_mock, strategy_name, n_traded_assets):
+    def __init__(self, is_mock, strategy_name, n_traded_assets, g_worksheet=None):
         # Hyperparameters
+        if is_mock and g_worksheet is None:
+            raise ValueError("'g_worksheet' is required when 'is_mock' is True")
         self.is_mock = is_mock
+        self.g_worksheets_mock = g_worksheet
         self.strategy_name = strategy_name
         self.n_traded_assets = n_traded_assets
 
@@ -20,6 +23,14 @@ class TradingDesk:
         # Objects
         self.position_calculator = PositionCalculator(strategy_name = self.strategy_name,
                                                       n_traded_assets = self.n_traded_assets)
+
+        # Initialization
+        if self.is_mock:
+            # Create new google spreadsheet page and initialize
+            pass
+        else:
+            # Clear positions in binance account
+            pass
 
         print("TradingDesk is created.")
 
@@ -51,6 +62,7 @@ class TradingDesk:
         Step 2
         : Position calculation
         """
+
         # data = fetch_data()
         positions = self.position_calculator.get_positions(data=None)
 
@@ -59,21 +71,28 @@ class TradingDesk:
         Step 3
         : Open new positions
         """
+
         for position in positions:
             if self.is_mock:
                 pass
                 # if position.position == 1:
-                #     place_mock_market_buy(symbol=position.symbol)
+                #     res = place_mock_market_buy(symbol=position.symbol)
                 # elif positions.position == -1:
-                #     place_mock_market_sell(symbol=position.symbol)
-                # self.positions_holding.append(position)
+                #     res = place_mock_market_sell(symbol=position.symbol)
+                # if res == success:
+                #     self.positions_holding.append(position)
+                # else:
+                #     logger.warning()
             else:
                 pass
                 # if positions.position == 1:
-                #     place_market_buy(symbol=position.symbol)
+                #     res = place_market_buy(symbol=position.symbol)
                 # elif positions.position == -1:
-                #     place_market_sell(symbol=position.symbol)
-                # self.positions_holding.append(position)
+                #     res = place_market_sell(symbol=position.symbol)
+                # if res == success:
+                #     self.positions_holding.append(position)
+                # else:
+                #     logger.warning()
 
     def run_strategy(self):
         self.strategy_func()
