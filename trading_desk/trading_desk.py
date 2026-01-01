6b3868b540315d0496c154e962858db53e1a6b77
Rouxist <name.of.squaner@gmail.com>
2026-01-01 13:58:49 +0000

    feat: added logger

--- trading_desk/trading_desk.py
+++ trading_desk/trading_desk.py
@@ -1,4 +1,6 @@
 import datetime
+import logging
+from logging.handlers import RotatingFileHandler
 
 from .strategy import PositionCalculator
 
@@ -25,17 +27,41 @@ class TradingDesk:
                                                       n_traded_assets = self.n_traded_assets)
 
         # Initialization
+        ## Logger
+        self.logger = logging.getLogger("my_app")
+        self.logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter(
+            "%(asctime)s :: [%(levelname)s] :: %(message)s"
+        )
+
+        ### File handler setup
+        file_handler = RotatingFileHandler(
+            f"./logs/{strategy_name}__{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.log",
+            maxBytes=1024*1024*10,  # 10 MB
+            backupCount=5           # up to 5 log files
+        )
+        file_handler.setFormatter(formatter)
+
+        ### Console handler setup
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
+
+        ## Account
         if self.is_mock:
             # Create new google spreadsheet page and initialize
             pass
         else:
             # Clear positions in binance account
             pass
-
-        print("TradingDesk is created.")
+        
+        self.logger.info("TradingDesk is created.")
 
     def strategy_func(self):
-        print("strategy_func is executed at:", datetime.datetime.now())
+        self.logger.info("strategy_func is executed")
 
         """
         Step 1
@@ -43,20 +69,27 @@ class TradingDesk:
         """
         
         if self.positions_holding: # If positions_holding is not empty
-            for ticker in self.positions_holding:
+            for position in self.positions_holding:
                 if self.is_mock:
                     pass
-                    # if position is buy:
-                    #     place_mock_market_sell()
+                    # if position.position == 1:
+                    #     res = place_mock_market_sell(symbol=position.symbol)
+                    # elif positions.position == -1:
+                    #     res = place_mock_market_buy(symbol=position.symbol)
+                    # if res == success:
+                    #     self.positions_holding.remove(position)
                     # else:
-                    #     place_mock_market_buy()
+                    #     self.logger.info(f"mock {position.position} order of {position.symbol} was not successfully cleared")
                 else:
                     pass
-                    # if position is buy:
-                    #     place_market_sell()
+                    # if positions.position == 1:
+                    #     res = place_market_sell(symbol=position.symbol)
+                    # elif positions.position == -1:
+                    #     res = place_market_buy(symbol=position.symbol)
+                    # if res == success:
+                    #     self.positions_holding.append(position)
                     # else:
-                    #     place_market_buy()
-
+                    #     self.logger.info(f"{position.position} order of {position.symbol} was not successfully cleared")
 
         """
         Step 2
@@ -82,7 +115,7 @@ class TradingDesk:
                 # if res == success:
                 #     self.positions_holding.append(position)
                 # else:
-                #     logger.warning()
+                #     self.logger.info(f"mock {position.position} order of {position.symbol} was not successfully executed")
             else:
                 pass
                 # if positions.position == 1:
@@ -92,7 +125,7 @@ class TradingDesk:
                 # if res == success:
                 #     self.positions_holding.append(position)
                 # else:
-                #     logger.warning()
+                #     self.logger.info(f"{position.position} order of {position.symbol} was not successfully executed")
 
     def run_strategy(self):
         self.strategy_func()
