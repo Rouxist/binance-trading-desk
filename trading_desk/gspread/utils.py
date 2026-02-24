6939018f57993e0b4491f63fe8e21818b6339f22
Rouxist <name.of.squaner@gmail.com>
2026-02-24 12:07:29 +0000

    feat: add function to update gspread worksheet cells

--- trading_desk/gspread/utils.py
+++ trading_desk/gspread/utils.py
@@ -3,3 +3,10 @@ def get_cell_value(worksheet,
     res = worksheet.acell(cell).value
 
     return res
+
+def update_cell_value(worksheet,
+                      cell:str,
+                      value):
+    worksheet.update([[value]], cell)
+
+    return res
